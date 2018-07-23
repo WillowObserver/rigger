f88b4b590386fa0df6f36521141f061d1dba46ba
WillowObserver <38207982+WillowObserver@users.noreply.github.com>
2018-07-23 23:18:42 +0800

    1

--- src/main/java/wang/lei/rigger/Config/DriudConfig.java
+++ src/main/java/wang/lei/rigger/Config/DriudConfig.java
@@ -19,7 +19,7 @@ public class DriudConfig {
 		// 创建servlet注册实体
 		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),
 				"/druid/*");
-		// 设置ip白名单
+		// 设置ip白名单,此ip下可以访问到/druid/index.html连接,其他ip不可访问
 		servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
 		// 设置ip黑名单,如果allow与deny共同存在时,deny优先于allow
 		servletRegistrationBean.addInitParameter("deny", "192.168.0.1");
